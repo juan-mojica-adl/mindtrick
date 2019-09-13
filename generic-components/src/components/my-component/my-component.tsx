daa4c9335c7e702dc9f2c4098e8fa4bf62cb3549
Juan Camilo Mojica <juan.mojica@avaldigitallabs.com>
2019-09-13 17:39:53 -0500

    mas avances

--- generic-components/src/components/my-component/my-component.tsx
+++ generic-components/src/components/my-component/my-component.tsx
@@ -23,16 +23,26 @@ export class MyComponent {
    */
   @Prop() last: string;
 
+
   private textFormato?: HTMLInputElement
-  private textInitYear?: HTMLInputElement
-  private textInitMonth?: HTMLInputElement
-  private textInitDay?: HTMLInputElement
+  private textInitYear?: HTMLSelectElement
+  private textInitMonth?: HTMLSelectElement
+  private textInitDay?: HTMLSelectElement
 
-  private textEndYear?: HTMLInputElement
-  private textEndMonth?: HTMLInputElement
-  private textEndDay?: HTMLInputElement
+  private textEndYear?: HTMLSelectElement
+  private textEndMonth?: HTMLSelectElement
+  private textEndDay?: HTMLSelectElement
 
-  private initialDateLabel;
+  private initialDateLabel?: HTMLInputElement
+  private finalDateLabel?: HTMLInputElement
+  private years;
+  private months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+  private days;
+
+  constructor(){
+    this.getYears(1990, 2020)
+    this.getDays();
+  }
 
   private getText(): string {
     return format(this.first, this.middle, this.last);
@@ -78,7 +88,7 @@ export class MyComponent {
     this.deleteEndMonth(e);
     this.deleteEndDay(e);
     this.toggleEnabledEndYear(e);
-    this.formatInitialDate()
+    this.formatInitialDate();
   }
 
   toggleEnabledEndYear(e){
@@ -118,19 +128,53 @@ export class MyComponent {
     }
   }
 
+  executeLastValidation(e){
+    this.formatEndDate();
+  }
+
+  formatDates(e){
+    this.formatInitialDate();
+    this.formatEndDate();
+  }
+
   formatInitialDate(){
     let initialDate = this.textInitYear.value + "-" + this.textInitMonth.value + "-" + this.textInitDay.value;
-    console.log("fec", initialDate)
     if(this.textInitDay){
-      this.initialDateLabel = moment(initialDate).zone(-5).format(this.textFormato.value != "" ? this.textFormato.value : "YYYY-MM-DD");
-      if (!moment(this.initialDateLabel, this.textFormato.value).isValid()){
-        this.initialDateLabel = "Fecha o formato no válidos";
+      this.initialDateLabel.value = moment(initialDate).zone(-5).format(this.textFormato.value != "" ? this.textFormato.value : "YYYY-MM-DD");
+      if (!moment(this.initialDateLabel.value, this.textFormato.value).isValid()){
+        this.initialDateLabel.value = "Fecha o formato no válidos";
       }
     }else{
-      this.initialDateLabel =  "";
+      this.initialDateLabel.value =  "";
     }
-
     console.log("fec2", this.initialDateLabel)
+  }
+
+  formatEndDate(){
+    let endDate = this.textEndYear.value + "-" + this.textEndMonth.value + "-" + this.textEndDay.value;
+    if(this.textEndDay){
+      this.finalDateLabel.value = moment(endDate).zone(-5).format(this.textFormato.value != "" ? this.textFormato.value : "YYYY-MM-DD");
+      if (!moment(this.finalDateLabel.value, this.textFormato.value).isValid()){
+        this.finalDateLabel.value = "Fecha o formato no válidos";
+      }
+    }else{
+      this.finalDateLabel.value =  "";
+    }
+    console.log("fec2", this.finalDateLabel)
+  }
+
+  getYears(minYear, maxYear){
+    this.years = new Array();
+    for (var i = minYear; i<=maxYear; i++){
+      this.years.push(i);
+    }
+  }
+
+  getDays(){
+    this.days = new Array();
+    for (var i=1; i<=31; i++){
+      this.days.push(i);
+    }
   }
 
   render() {
@@ -138,20 +182,52 @@ export class MyComponent {
     <div>
       <p>
         Digite el formato de las fechas:  &nbsp;
-        <input ref={el => this.textFormato = el as HTMLInputElement } onKeyPress={(e)=>this.deleteEndYear(e)} id="formato" type="text" placeholder="Formato fecha"></input>
+        <input ref={el => this.textFormato = el as HTMLInputElement } onKeyPress={(e)=>this.formatDates(e)} id="formato" type="text" placeholder="Formato fecha"></input>
       </p>
       <p>
-        <input ref={el => this.textInitYear = el as HTMLInputElement } id="initYear" type="text" placeholder="yyyy" onKeyPress={(e)=>this.deleteInitialMonth(e)}></input>
-        <input ref={el => this.textInitMonth = el as HTMLInputElement } disabled={true} onKeyPress={(e)=>this.deleteInitialDay(e)} id="initMonth" name= "mon" type="text" placeholder="MM"></input>
-        <input ref={el => this.textInitDay = el as HTMLInputElement } disabled={true} onKeyPress={(e)=>this.deleteEndYear(e)} id="initDay" type="text" placeholder="DD"></input>
-        <div>{this.initialDateLabel}</div>
+        <select ref={el => this.textInitYear = el as HTMLSelectElement } id="initYear" onChange={(e)=>this.deleteInitialMonth(e)}>
+          { this.years.map((year)=>
+            <option value={year}>{year}</option>
+          )}
+        </select>
+        &nbsp;
+        <select ref={el => this.textInitMonth = el as HTMLSelectElement } disabled={true} onChange={(e)=>this.deleteInitialDay(e)} id="initMonth" name= "mon">
+          { this.months.map((month)=>
+            <option value={this.months.indexOf(month)+1}>{month}</option>
+          )}
+        </select>
+        &nbsp;
+        <select ref={el => this.textInitDay = el as HTMLSelectElement } disabled={true} onChange={(e)=>this.deleteEndYear(e)} id="initDay">
+          { this.days.map((day)=>
+            <option value={day}>{day}</option>
+          )}
+        </select>
+        &nbsp;
+        &nbsp;
+        <input ref={el => this.initialDateLabel = el as HTMLInputElement } type="text" readOnly={true}></input>
       </p>
       <p>
-        <input ref={el => this.textEndYear = el as HTMLInputElement } disabled={true} onKeyPress={(e)=>this.deleteEndMonth(e)} id="endYear" type="text" placeholder="yyyy"></input>
-        <input ref={el => this.textEndMonth = el as HTMLInputElement } disabled={true} onKeyPress={(e)=>this.deleteEndDay(e)} id="endMonth" type="text" placeholder="MM"></input>
-        <input ref={el => this.textEndDay = el as HTMLInputElement } disabled={true} id="endDay" type="text" placeholder="DD"></input>
+        <select ref={el => this.textEndYear = el as HTMLSelectElement } disabled={true} onChange={(e)=>this.deleteEndMonth(e)} id="endYear">
+          { this.years.map((year)=>
+            <option value={year}>{year}</option>
+          )}
+        </select>
+        &nbsp;
+        <select ref={el => this.textEndMonth = el as HTMLSelectElement } disabled={true} onChange={(e)=>this.deleteEndDay(e)} id="endMonth">
+          { this.months.map((month)=>
+            <option value={this.months.indexOf(month)+1}>{month}</option>
+          )}
+        </select>
+        &nbsp;
+        <select ref={el => this.textEndDay = el as HTMLSelectElement } disabled={true} onChange={(e)=>this.executeLastValidation(e)} id="endDay">
+          { this.days.map((day)=>
+            <option value={day}>{day}</option>
+          )}
+        </select>
+        &nbsp;
+        &nbsp;
+        <input ref={el => this.finalDateLabel = el as HTMLInputElement } type="text" readOnly={true}></input>
       </p>
     </div>)
   }
-
 }
